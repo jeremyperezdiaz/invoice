d06e8acc9f6af76ae88c726aeb0179d1982848b5
jeremyperezdiaz <jeremy@ozeros.com>
2020-08-08 18:32:44 -0400

    filtro de invoice por fecha

--- invoice_lista.php
+++ invoice_lista.php
@@ -1,38 +1,63 @@
 <?php include 'header.php'; ?>
 <?php include 'cn.php'; ?>
 
-<div class="section container">
-    <h5 class="header left blue-text text-darken-2">Buscar INVOICE</h5>
-    <div class="row">
-        <form action="invoice_base_PDF.php" method="POST" class="col s12">
-            <div class="input field col s3">
-                <label>Seleccionar el INVOICE</label>
-                <select name="invoice" id="invoice">
-                    <!-- <option value="" disabled selected>Lista de Emisores</option> -->
-                    <?php
-                    $sql = "SELECT idInvoice,fecha, c.nombre as nombre FROM invoice i
-                                INNER JOIN cliente c ON i.idCliente = c.idCliente
-                                ORDER BY idInvoice DESC LIMIT 10";
-                    $resultado = mysqli_query($conexion, $sql);
+<?php
+// Si el post viene vacio carga fecha de hoy por defecto 
+if (!empty($_POST["fechaInicio"]) and !empty($_POST["fechaFin"])):
+    $fechaInicio = $_POST["fechaInicio"];
+    $fechaFin = $_POST["fechaFin"];
+    $sql = "SELECT i.idInvoice as idInvoice, i.fecha AS fecha, e.nombre AS nombreEmisor, c.nombre AS nombreCliente, c.contacto as contacto,
+        d.valor as valor, est.descripcion as estadoPago, i.total as total, item.descripcion AS itemDescripcion, d.descripcion as itemDetalle
+        FROM invoice i
+        INNER JOIN cliente c ON i.idCliente = c.idCliente
+        INNER JOIN emisor e ON i.idEmisor = e.idEmisor
+        INNER JOIN estado est ON i.idEstado = est.idEstado
+        INNER JOIN detalle d ON d.idInvoice = i.idInvoice
+        INNER JOIN item ON item.idItem = d.idItem
+        WHERE fecha BETWEEN '$fechaInicio' and '$fechaFin'
+        ORDER BY idInvoice DESC LIMIT 10";
+else:
+    $sql = "SELECT i.idInvoice as idInvoice, i.fecha AS fecha, e.nombre AS nombreEmisor, c.nombre AS nombreCliente, c.contacto as contacto,
+        d.valor as valor, est.descripcion as estadoPago, i.total as total, item.descripcion AS itemDescripcion, d.descripcion as itemDetalle
+        FROM invoice i
+        INNER JOIN cliente c ON i.idCliente = c.idCliente
+        INNER JOIN emisor e ON i.idEmisor = e.idEmisor
+        INNER JOIN estado est ON i.idEstado = est.idEstado
+        INNER JOIN detalle d ON d.idInvoice = i.idInvoice
+        INNER JOIN item ON item.idItem = d.idItem
+        ORDER BY idInvoice DESC LIMIT 10";
+endif;
 
-                    while ($lista = mysqli_fetch_array($resultado)) {
-                    ?>
-                        <option value="<?php echo $lista['idInvoice'] ?>"><?php echo 'Nº ' . $lista['idInvoice'] . ' - ' . $lista['nombre'] ?></option>
-                    <?php
-                    };
-                    ?>
-                </select>
+?>
+
+<div class="section container">
+    <h5 class="header left blue-text text-darken-2">Buscar INVOICE Filtrando por fecha</h5>
+    <div class="row">
+        <form method="POST" class="col s12">
+            <div class="input field col s3">
+                <label for="fecha">Desde:</label>
+                <input type="text" class="datepickerInicio" id="fechaInicio" name="fechaInicio">
+            </div>
+            <div class="input field col s3">
+                <label for="fecha">Hasta:</label>
+                <input type="text" class="datepickerFin" id="fechaFin" name="fechaFin">
             </div>
             <div class="input field s1">
                 </br>
-                <button class="btn red darken-3 waves-effect waves-light" type="submit" name="action">Descargar
-                    <i class="material-icons right">picture_as_pdf</i>
+                <button class="btn blue darken-3 waves-effect waves-light" type="submit" name="action">cargar por fecha
+                    <i class="material-icons right">file_download</i>
                 </button>
             </div>
         </form>
-    </div>
 
-    <h5 class="blue-text darken-2">Listado de Invoices</h5>
+    </div>
+                
+    <h5 class="blue-text">Listado de Invoices
+        <?php if(!empty($_POST)):
+            echo '('.$fechaInicio.' y '.$fechaFin.')';
+        endif;
+        ?>
+    </h5>
     <table class="highlight centered" class="responsive-table" id="tablaInvoices">
         <thead>
             <tr>
@@ -50,16 +75,8 @@
             </tr>
         </thead>
         <?php
-        $sql = "SELECT i.idInvoice as idInvoice, i.fecha AS fecha, e.nombre AS nombreEmisor, c.nombre AS nombreCliente, c.contacto as contacto,
-        d.valor as valor, est.descripcion as estadoPago, i.total as total, item.descripcion AS itemDescripcion, d.descripcion as itemDetalle
-        FROM invoice i
-        INNER JOIN cliente c ON i.idCliente = c.idCliente
-        INNER JOIN emisor e ON i.idEmisor = e.idEmisor
-        INNER JOIN estado est ON i.idEstado = est.idEstado
-        INNER JOIN detalle d ON d.idInvoice = i.idInvoice
-        INNER JOIN item ON item.idItem = d.idItem
-        /*WHERE fecha BETWEEN fechaInicio and fechaTermino*/
-        ORDER BY idInvoice DESC LIMIT 10";
+
+        //Se ejecuta el SQL que se cargará a la tabla
         $resultado = mysqli_query($conexion, $sql);
 
         while ($lista = mysqli_fetch_array($resultado)) {
@@ -77,7 +94,7 @@
                 <td><?php echo $lista['estadoPago'] ?></td>
                 <td>
                     <form action="invoice_base_PDF.php" method="POST">
-                        <!-- Boton hace PDF -->
+                        <!-- Boton PDF -->
                         <button name="invoice" id="invoice" value="<?php echo $lista['idInvoice'] ?>" class="btn-floating red darken-2 waves-effect waves-light
                             tooltipped" data-position="right" data-tooltip="Crear PDF Invoice N° <?php echo $lista['idInvoice'] ?>" type="submit"><i class="material-icons right">picture_as_pdf</i>
                         </button>
